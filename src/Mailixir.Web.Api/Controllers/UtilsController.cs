bd0a44acb40a8c0ee4a22d5582b3f44c4fe547b8
Javier Romero <deromero@gmail.com>
2021-09-01 12:54:41 -0500

    setup serilog functionality

---
+++ src/Mailixir.Web.Api/Controllers/UtilsController.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using Mailixir.Web.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Mailixir.Web.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UtilsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<UtilsController> _logger;
+
+        public UtilsController(IConfiguration configuration, ILogger<UtilsController> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("ping")]
+        public async Task<IActionResult> Ping()
+        {
+            _logger.LogInformation("Starts UTILS/PING");
+
+            var response = new ApiResponse
+            {
+                Message = "MAILIXIR SERVICE WORKING OK",
+                Code = "000"
+            };
+
+            return Ok(response);
+
+        }
+
+    }
+}
---
+++ src/Mailixir.Web.Api/Models/ApiResponse.cs
@@ -0,0 +1,8 @@
+namespace Mailixir.Web.Api.Models
+{
+    public class ApiResponse
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+    }
+}
